13cd8a7f1829544d161a2ae45dc876084694ec0f
IlyaToritsyn <toritsyn41@gmail.com>
2020-06-05 01:15:53 +0300

    Change a disign. Add a clock.

--- Control/Form1.Designer.cs
+++ Control/Form1.Designer.cs
@@ -28,31 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.userControl11 = new Control.UserControl1();
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.clockLabel = new System.Windows.Forms.Label();
+            this.buttonTimer = new System.Windows.Forms.Button();
+            this.numericUpDownSeconds = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownMinutes = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownHours = new System.Windows.Forms.NumericUpDown();
+            this.labelClockName = new System.Windows.Forms.Label();
+            this.labelTimer = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHours)).BeginInit();
             this.SuspendLayout();
             // 
-            // userControl11
+            // timer
             // 
-            this.userControl11.Location = new System.Drawing.Point(13, 13);
-            this.userControl11.Name = "userControl11";
-            this.userControl11.Size = new System.Drawing.Size(150, 150);
-            this.userControl11.TabIndex = 0;
+            this.timer.Enabled = true;
+            this.timer.Interval = 500;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // clockLabel
+            // 
+            this.clockLabel.AutoSize = true;
+            this.clockLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clockLabel.Location = new System.Drawing.Point(12, 50);
+            this.clockLabel.Name = "clockLabel";
+            this.clockLabel.Size = new System.Drawing.Size(0, 73);
+            this.clockLabel.TabIndex = 1;
+            // 
+            // buttonTimer
+            // 
+            this.buttonTimer.BackColor = System.Drawing.Color.OrangeRed;
+            this.buttonTimer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonTimer.Location = new System.Drawing.Point(285, 84);
+            this.buttonTimer.Name = "buttonTimer";
+            this.buttonTimer.Size = new System.Drawing.Size(143, 34);
+            this.buttonTimer.TabIndex = 7;
+            this.buttonTimer.Text = "Пуск";
+            this.buttonTimer.UseVisualStyleBackColor = false;
+            this.buttonTimer.Click += new System.EventHandler(this.ButtonTimer_Click);
+            // 
+            // numericUpDownSeconds
+            // 
+            this.numericUpDownSeconds.Location = new System.Drawing.Point(385, 56);
+            this.numericUpDownSeconds.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDownSeconds.Name = "numericUpDownSeconds";
+            this.numericUpDownSeconds.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDownSeconds.TabIndex = 6;
+            // 
+            // numericUpDownMinutes
+            // 
+            this.numericUpDownMinutes.Location = new System.Drawing.Point(334, 56);
+            this.numericUpDownMinutes.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDownMinutes.Name = "numericUpDownMinutes";
+            this.numericUpDownMinutes.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDownMinutes.TabIndex = 5;
+            // 
+            // numericUpDownHours
+            // 
+            this.numericUpDownHours.Location = new System.Drawing.Point(285, 56);
+            this.numericUpDownHours.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.numericUpDownHours.Name = "numericUpDownHours";
+            this.numericUpDownHours.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDownHours.TabIndex = 4;
+            // 
+            // labelClockName
+            // 
+            this.labelClockName.AutoSize = true;
+            this.labelClockName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelClockName.Location = new System.Drawing.Point(99, 15);
+            this.labelClockName.Name = "labelClockName";
+            this.labelClockName.Size = new System.Drawing.Size(66, 24);
+            this.labelClockName.TabIndex = 8;
+            this.labelClockName.Text = "ЧАСЫ";
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTimer.Location = new System.Drawing.Point(312, 15);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(95, 24);
+            this.labelTimer.TabIndex = 9;
+            this.labelTimer.Text = "ТАЙМЕР";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.userControl11);
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(455, 141);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.labelClockName);
+            this.Controls.Add(this.buttonTimer);
+            this.Controls.Add(this.numericUpDownSeconds);
+            this.Controls.Add(this.numericUpDownMinutes);
+            this.Controls.Add(this.numericUpDownHours);
+            this.Controls.Add(this.clockLabel);
+            this.ForeColor = System.Drawing.SystemColors.Window;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Часы и таймер";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHours)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private UserControl1 userControl11;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label clockLabel;
+        private System.Windows.Forms.Button buttonTimer;
+        private System.Windows.Forms.NumericUpDown numericUpDownSeconds;
+        private System.Windows.Forms.NumericUpDown numericUpDownMinutes;
+        private System.Windows.Forms.NumericUpDown numericUpDownHours;
+        private System.Windows.Forms.Label labelClockName;
+        private System.Windows.Forms.Label labelTimer;
     }
 }
 
--- Control/Form1.cs
+++ Control/Form1.cs
@@ -1,20 +1,98 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Control
 {
     public partial class Form1 : Form
     {
+        private bool IsTimerEnabled { get; set; } = false;
+
+        private DateTime TimerOff { get; set; }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Умен. времени в таймере.
+        /// </summary>
+        private void UpdateTimer()
+        {
+            ////Умен. сек.
+            //if (numericUpDownSeconds.Value > 0)
+            //{
+            //    numericUpDownSeconds.Value--;
+            //}
+
+            ////Умен. мин и сек.
+            //else if (numericUpDownMinutes.Value > 0)
+            //{
+            //    numericUpDownMinutes.Value--;
+            //    numericUpDownSeconds.Value = 59;
+            //}
+
+            ////Умен. часы, мин и сек.
+            //else if (numericUpDownHours.Value > 0)
+            //{
+            //    numericUpDownHours.Value--;
+            //    numericUpDownMinutes.Value = 59;
+            //    numericUpDownSeconds.Value = 59;
+            //}
+
+            ////Если время вышло.
+            //else if (numericUpDownHours.Value == 0 && numericUpDownMinutes.Value == 0 && numericUpDownSeconds.Value == 0)
+            //{
+            //    IsTimerEnabled = false;
+
+            //    MessageBox.Show("Ваше время вышло.");
+            //}
+
+            if (TimerOff <= DateTime.Now)
+            {
+                IsTimerEnabled = false;
+
+                MessageBox.Show("Время вышло.");
+            }
+
+            else
+            {
+                TimeSpan lastTime = TimerOff - DateTime.Now;
+                numericUpDownHours.Value = lastTime.Hours;
+                numericUpDownMinutes.Value = lastTime.Minutes;
+                numericUpDownSeconds.Value = lastTime.Seconds;
+            }
+        }
+
+        /// <summary>
+        /// Метод элемента Timer.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            clockLabel.Text = DateTime.Now.ToLongTimeString();
+
+            if (IsTimerEnabled)
+            {
+                UpdateTimer();
+            }
+        }
+
+        /// <summary>
+        /// Если нажата кнопка Пуск.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonTimer_Click(object sender, EventArgs e)
+        {
+            TimerOff = DateTime.Now;
+
+            TimerOff = TimerOff.AddHours((double)numericUpDownHours.Value);
+            TimerOff = TimerOff.AddMinutes((double)numericUpDownMinutes.Value);
+            TimerOff = TimerOff.AddSeconds((double)numericUpDownSeconds.Value);
+
+            IsTimerEnabled = true;
         }
     }
 }
